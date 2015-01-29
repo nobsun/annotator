c636e5ea4034b05651e0224d517b5a040b91ca10
Nobuo Yamashita <nobsun@sampou.org>
2015-01-29 22:28:08 +0900

    Annotator

---
+++ src/Language/Annotator.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+-- |
+-- Module      : Language.Annotator
+-- Description : Annotator for Syntax Tree
+-- Copyright   : (c) Nobuo Yamashita, 2015
+-- License     : BSD3
+-- Maintainer  : nobsun@sampou.org
+-- Stability   : experimental
+module Language.Annotator (
+    -- * Classes
+     Annotator (..)
+    ,Annotatable (..)
+    -- * Newtype
+    ,I
+    ) where
+
+import Control.Comonad (Comonad (extract))
+
+-- |
+-- Equivalent for Control.Monad.Identity.Identity
+newtype I a
+  = I { unI :: a -- ^ unwraping 
+      }
+
+-- |
+-- Annotator class
+class Comonad fa => Annotator fa where
+  -- | getting annotating type
+  type Annotation fa 
+  -- | getting annotating
+  annotating :: forall b . fa b -> Annotation fa
+  -- | getting annotated
+  annotated :: forall b . fa b -> b
+  annotated =  extract
+
+
+instance Annotator ((,) a) where
+  type Annotation ((,) a) = a
+  annotating = fst
+
+-- |
+-- Annotatable class
+class Annotator f => Annotatable t f where
+  annotate   :: t I a -> f (t f a)
+  unannotate :: f (t f a) -> t I a 
+
